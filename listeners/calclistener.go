ebba9cf22336bdc8ee63f12651d246b5348ed026
maksym.pecheniuk <maksym.pecheniuk@zorallabs.com>
2024-12-19 17:18:58 +0200

    clean code a bit

--- listeners/calclistener.go
+++ listeners/calclistener.go
@@ -21,30 +21,53 @@ type Response struct {
 func ListenNatsCalcRequests() {
 	nc, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Failed to connect to NATS: %v", err)
 	}
 	defer nc.Close()
-
-	_, subErr := nc.Subscribe(CalcSubject, func(m *nats.Msg) {
-		log.Printf("Received a request: %s", string(m.Data))
-		var request Request
-		jsonErr := json.Unmarshal(m.Data, &request)
-		if jsonErr != nil {
-			log.Printf("Error decoding JSON: %v", err)
-			_ = m.Respond([]byte(`{"error": "Invalid JSON format"}`))
-			return
-		}
-		result, jsonErr := calculator.Calculate(request.Expression)
-		res, _ := json.Marshal(Response{Result: result})
-		err := m.Respond(res)
-		if err != nil {
-			log.Printf("Error responding to message: %v", err)
-		}
+	_, err = nc.Subscribe(CalcSubject, func(m *nats.Msg) {
+		handleCalcRequest(m)
 	})
-	if subErr != nil {
-		log.Printf("Error subscribing to subject 'hello': %v", err)
-		return
+	if err != nil {
+		log.Fatalf("Failed to subscribe to subject '%s': %v", CalcSubject, err)
 	}
 	log.Printf("Listening for requests on subject: '%s'...", CalcSubject)
 	select {}
+}
+
+func handleCalcRequest(m *nats.Msg) {
+	log.Printf("Received request: %s", string(m.Data))
+	var request Request
+	if err := json.Unmarshal(m.Data, &request); err != nil {
+		sendErrorResponse(m, "Invalid JSON format")
+		log.Printf("Error parsing JSON: %v", err)
+		return
+	}
+	result, err := calculator.Calculate(request.Expression)
+	if err != nil {
+		sendErrorResponse(m, err.Error())
+		log.Printf("Error calculating expression: %v", err)
+		return
+	}
+	sendSuccessResponse(m, result)
+}
+
+func sendErrorResponse(m *nats.Msg, errorMsg string) {
+	response := Response{Error: errorMsg}
+	sendResponse(m, response)
+}
+
+func sendSuccessResponse(m *nats.Msg, result float64) {
+	response := Response{Result: result}
+	sendResponse(m, response)
+}
+
+func sendResponse(m *nats.Msg, response Response) {
+	resBytes, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Failed to serialize response: %v", err)
+		return
+	}
+	if err := m.Respond(resBytes); err != nil {
+		log.Printf("Failed to send response: %v", err)
+	}
 }
